87d66f9ceb42f5449a7b5e43eebee1f24ff50321
Muhammad Arslan <thearslan380@gmail.com>
2020-06-17 07:31:17 -0700

    finished the routes

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 ActiveRecord::Schema.define(version: 2020_06_17_110501) do
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "address"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
