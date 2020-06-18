9791dbe73ec7a15fafa3cfea6d12d5aa1e54876b
Muhammad Arslan <thearslan380@gmail.com>
2020-06-17 21:58:57 -0700

    add model relations, scopes and updated views

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,40 +1,40 @@
 class EventsController < ApplicationController
-    def index
-      @events = Event.all
+  def index
+    @events = Event.all
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def create
+    begin
+      @user = User.find(session[:user_id])
+    rescue ActiveRecord::RecordNotFound
+      @user = nil
     end
-  
-    def new
-      @event = Event.new
-    end
-  
-    def show
-      @event = Event.find(params[:id])
-    end
-  
-    def create
-      begin
-        @user = User.find(session[:user_id])
-      rescue ActiveRecord::RecordNotFound
-        @user = nil
-      end
-  
-      if @user
-        @event = @user.created_events.build(event_params)
-        @user.save
-  
-        if @event.persisted?
-          redirect_to event_path(@event)
-        else
-          render :new
-        end
+
+    if @user
+      @event = @user.created_events.build(event_params)
+      @user.save
+
+      if @event.persisted?
+        redirect_to event_path(@event)
       else
-        redirect_to sign_in_path
+        render :new
       end
-    end
-  
-    private
-  
-    def event_params
-      params.require(:event).permit(:title, :description, :date)
+    else
+      redirect_to sign_in_path
     end
   end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :description, :date)
+  end
+end
