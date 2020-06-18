3e3f48a8e553b209998a4113fbd762a72061e318
Muhammad Arslan <thearslan380@gmail.com>
2020-06-17 22:02:41 -0700

    added the navigation

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   resources :users
-  resources :user, only: [:new, :create, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :user, only: [:new, :create, :show]
+  resources :events
   get "/sign_in", to: "users#sign_in_view"
   post "/sign_in", to: "users#sign_in"
+
+  root "events#index"
 end
